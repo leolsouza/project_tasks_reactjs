ed1463c712811bdaf9d12db8fa525d61210fe491
Leonardo Lima <eng.leolsouza@hotmail.com>
2022-06-23 13:51:08 -0300

    adding_loading_in_page

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -6,20 +6,24 @@ import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./Page.css";
+import Loading from "./Loading";
 
 const Page = () => {
   const [tasks, setTasks] = useState([]);
+  const [removeLoading, setRemoveLoading] = useState(false);
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      
-      const { data } = await axios.get("http://127.0.0.1:8000/api/tasks");
-      
-      setTasks(data);
-    };
-
-    fetchTasks();
-    
+    setTimeout(() => {
+      const fetchTasks = async () => {
+        
+        const { data } = await axios.get("http://127.0.0.1:8000/api/tasks");
+        
+        setTasks(data);
+        setRemoveLoading(true);
+      };
+  
+      fetchTasks();
+    },2000)
   }, []);
 
   const handleTaskClick = (taskId) => {
@@ -59,7 +63,8 @@ const Page = () => {
         handleTaskClick={handleTaskClick}
         handleTaskDeletion={handleTaskDeletion}
       />
-    </div>
+      {!removeLoading && <Loading />}
+    </div>  
   );
 };
 
